07907bb1dcadadbcf8d6ba62127469154632e663
Patrick McClernan <patmccler@gmail.com>
2017-10-25 21:57:58 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,6 +2,13 @@
 function forLoop(array) {
   for(let i = 0; i < 25; i++)
   {
-    array[i] = `I am ${i} strange loops`
+    if( i === 1)
+    {
+      array.push("I am 1 strange loop.")
+    }
+    else
+    {
+      array[i] = `I am ${i} strange loops`
+    }
   }
 }
